f4943f44f3ccda5e2f709c6d2602aa751875fcca
Takamichi Omori <merrytakamerry@gmail.com>
2019-09-14 13:48:21 +0900

    use firestore

---
+++ src/firebase/config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBN1GADeE7gxSyE3430uMDISgAIw5BVGrU",
+    authDomain: "co-create-323f8.firebaseapp.com",
+    databaseURL: "https://co-create-323f8.firebaseio.com",
+    projectId: "co-create-323f8",
+    storageBucket: "co-create-323f8.appspot.com",
+    messagingSenderId: "616467026239",
+    appId: "1:616467026239:web:f669254d6bba9af454038a"
+  };
+
+export default firebaseConfig
