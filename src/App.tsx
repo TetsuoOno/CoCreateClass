655380f643c6da1cfc35fb8a63d671b9afe83adf
Takamichi Omori <merrytakamerry@gmail.com>
2019-09-14 14:46:56 +0900

    add login

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Top from './components/top';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import Login from './components/login';
 
 const App: React.FC = () => {
   return (
     <Switch>
       <Route exact path='/' component={Top} />
+      <Route path='/login' component={Login} />
     </Switch>
   );
 }
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -4,4 +4,5 @@ import firebaseConfig from './config'
 firebase.initializeApp(firebaseConfig);
 
 export const fireStore = firebase.firestore()
+export const auth = firebase.auth()
 export const questionIndex = "questions"
